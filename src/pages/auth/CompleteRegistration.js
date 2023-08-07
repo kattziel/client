7a4ba8e05f17d7907ddf2122561e7ee7b9b4adbb
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-08-07 12:12:42 +0200

    useEffect with getting email from localStorage

--- src/pages/auth/CompleteRegistration.js
+++ src/pages/auth/CompleteRegistration.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
@@ -10,7 +10,63 @@ const CompleteRegistration = () => {
 
   const handleSubmit = () => {};
 
-  // const email = localStorage.getItem("email");
+  const navigate = useNavigate();
+  useEffect(() => {
+    setEmail(window.localStorage.getItem("emailFormRegistration"));
+  }, [navigate]);
+
+  return (
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card p-4">
+            <div className="card-body">
+              {loading ? (
+                <h4 className="text-danger">Loading...</h4>
+              ) : (
+                <h4 className="mb-4">Complete Registration</h4>
+              )}
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label>Email address</label>
+                  <input
+                    type="email"
+                    value={email}
+                    className="form-control"
+                    placeholder={email}
+                    disabled
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    placeholder={"Enter password"}
+                    disabled={loading}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary mt-3"
+                  disabled={!email || loading}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompleteRegistration;
+
+// const email = localStorage.getItem("email");
 
 //   return (
 //     <div className="container p-5">
@@ -37,43 +93,3 @@ const CompleteRegistration = () => {
 //     </div>
 //   );
 // };
-
-return (
-    <div className="container py-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card p-4">
-            <div className="card-body">
-              {loading ? (
-                <h4 className="text-danger">Loading...</h4>
-              ) : (
-                <h4 className="mb-4">Complete Registration</h4>
-              )}
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label>Email address</label>
-                  <input
-                    type="email"
-                    value={email}
-                    className="form-control"
-                    placeholder={email}
-                    disabled={true}
-                  />
-                </div>
-                <button
-                  className="btn btn-primary mt-3"
-                  disabled={!email || loading}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default CompleteRegistration;
