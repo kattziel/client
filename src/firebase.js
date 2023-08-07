a776a6be3b5f0ddf79894bc0d952e29d12e9825d
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-08-07 11:18:25 +0200

    actualisation of firebase.js and creating submitHandler in Register.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
-import * as firebase from "firebase";
+import { initializeApp } from 'firebase/app';
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCBOOU19tedM_f6xOykfJDfgbQVUU0ZLYA",
   authDomain: "react-native-course-67ad0.firebaseapp.com",
-  // databaseURL:
-  //   "https://react-native-course-67ad0-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "react-native-course-67ad0",
   storageBucket: "react-native-course-67ad0.appspot.com",
-  // messagingSenderId: "919202730935",
   appId: "1:919202730935:web:8a23037f255dac32934e32",
+  // messagingSenderId: "919202730935",
+  // databaseURL:
+  //   "https://react-native-course-67ad0-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
-// const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+ const auth = firebase.auth();
+ const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+ export {auth, googleAuthProvider};
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,14 +1,33 @@
 import React, { useState } from "react";
+import { auth } from "../../firebase";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const config = {
+      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDICRECT,
+      handleCodeInApp: true,
+    };
+    const result = await auth.sendSignInLinkToEmail(email, config);
+    console.log("result", result);
+
+    // show notification to user about sent email
+
+    //save user's email to local storage
+    window.localStorage.setItem("emailFormRegistration", email);
+
+    //clear the state
+    setEmail("");
+    setLoading(false);
+  };
 
   return (
     <div className="container p-5">
-      <h4>Login</h4>
+      <h4>Register</h4>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Email address</label>
@@ -24,6 +43,7 @@ const Register = () => {
         <button
           className="btn btn-raised btn-primary"
           disabled={!email || loading}
+          onClick={handleSubmit}
         >
           Submit
         </button>
